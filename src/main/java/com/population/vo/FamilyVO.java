871d123f2bf9a40920d3097307ad8796bb1079dc
Administrator <2812708612@qq.com>
2020-12-29 15:22:00 +0800

    测试

---
+++ src/main/java/com/population/vo/FamilyVO.java
@@ -0,0 +1,140 @@
+package com.population.vo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class FamilyVO {
+    private Integer famId;
+
+    private String familyName;
+
+    private String familyNumber;
+
+    private Integer familySex;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date familyBrithday;
+
+    private Integer familyVaccine;
+
+    private Integer familyLearn;
+
+    private Integer delId;
+
+    private Integer personal;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date gmtCreate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date gmtModified;
+
+    private String spareStr;
+
+    private String spareInt;
+
+    public Integer getFamId() {
+        return famId;
+    }
+
+    public void setFamId(Integer famId) {
+        this.famId = famId;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getFamilyNumber() {
+        return familyNumber;
+    }
+
+    public void setFamilyNumber(String familyNumber) {
+        this.familyNumber = familyNumber;
+    }
+
+    public Integer getFamilySex() {
+        return familySex;
+    }
+
+    public void setFamilySex(Integer familySex) {
+        this.familySex = familySex;
+    }
+
+    public Date getFamilyBrithday() {
+        return familyBrithday;
+    }
+
+    public void setFamilyBrithday(Date familyBrithday) {
+        this.familyBrithday = familyBrithday;
+    }
+
+    public Integer getFamilyVaccine() {
+        return familyVaccine;
+    }
+
+    public void setFamilyVaccine(Integer familyVaccine) {
+        this.familyVaccine = familyVaccine;
+    }
+
+    public Integer getFamilyLearn() {
+        return familyLearn;
+    }
+
+    public void setFamilyLearn(Integer familyLearn) {
+        this.familyLearn = familyLearn;
+    }
+
+    public Integer getDelId() {
+        return delId;
+    }
+
+    public void setDelId(Integer delId) {
+        this.delId = delId;
+    }
+
+    public Integer getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(Integer personal) {
+        this.personal = personal;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getSpareStr() {
+        return spareStr;
+    }
+
+    public void setSpareStr(String spareStr) {
+        this.spareStr = spareStr;
+    }
+
+    public String getSpareInt() {
+        return spareInt;
+    }
+
+    public void setSpareInt(String spareInt) {
+        this.spareInt = spareInt;
+    }
+}
